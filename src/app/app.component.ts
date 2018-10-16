52bf4befb965005b8f4406f7fe91dce3971f490a
Kernael92 <kernaelapuko5@gmail.com>
2018-10-16 12:33:54 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Goal} from './goal'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+  goals =[
+     new Goal(1, 'Watch Finding Nemo'),
+     new Goal(2, 'Buy cookies'),
+     new Goal(3, 'Get new phone case'),
+     new Goal(4, 'Get Dog food'),
+     new Goal(5, 'Solve math homework'),
+     new Goal(6, 'Plot my world domination plan'),
+ ]
 }
